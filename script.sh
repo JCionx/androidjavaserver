e63491612a2169f722e5120cbbe914eaec0b31b4
JCionx <92257741+JCionx@users.noreply.github.com>
2022-05-05 00:03:00 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,12 +6,10 @@ read -p "Do you agree with the Minecraft EULA? (https://account.mojang.com/docum
 case $response in [yY][eE][sS]|[yY]|[jJ]|'') 
     cd Server
     wget https://papermc.io/api/v2/projects/paper/versions/1.18.2/builds/317/downloads/paper-1.18.2-317.jar
-    wget https://raw.githubusercontent.com/JCionx/androidjavaserver/master/start.sh
-    chmod +x start.sh
-    bash start.sh
+    java -Xms1024M -Xmx1024M -jar paper-1.18.2-317.jar --nogui
     rm eula.txt
     wget https://raw.githubusercontent.com/JCionx/androidjavaserver/master/eula.txt
-    java -jar paper-1.18.2-317.jar
+    java -Xms1024M -Xmx1024M -jar paper-1.18.2-317.jar --nogui
     stop
     echo Installation complete!
     ;;
