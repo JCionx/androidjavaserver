9cf5cf3199e32ee34340ce5cd7873ad3c97e3a2f
JCionx <92257741+JCionx@users.noreply.github.com>
2022-05-22 22:50:29 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
 cd ~
-pkg install openjdk-17
-mkdir Server
+
 read -p "Do you agree with the Minecraft EULA? (https://account.mojang.com/documents/minecraft_eula) [Y/n] " response
 
 case $response in [yY][eE][sS]|[yY]|[jJ]|'') 
+    pkg install openjdk-17 -y
+    mkdir Server
     cd Server
     wget https://papermc.io/api/v2/projects/paper/versions/1.18.2/builds/317/downloads/paper-1.18.2-317.jar
     java -Xms1024M -Xmx1024M -jar paper-1.18.2-317.jar --nogui
@@ -12,10 +13,12 @@ case $response in [yY][eE][sS]|[yY]|[jJ]|'')
     java -Xms1024M -Xmx1024M -jar paper-1.18.2-317.jar --nogui
     stop
     echo Installation complete!
+    cd ~
+    rm install.sh
     ;;
     *)
     cd ~
-    rmdir server
+    rm install.sh
     echo Installation cancelled.
     ;;
 esac
